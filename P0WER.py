c5e652deaeb540b78aa6ab1d84cff37b5c6c0a67
SSG ONWER <122170600+MARWAN-TECH404@users.noreply.github.com>
2023-04-17 06:14:02 +0500

    Update P0WER.py

--- P0WER.py
+++ P0WER.py
@@ -1,4 +1,4 @@
-import os,platform
+import pOw3r
 os.system("xdg-open https://facebook.com/groups/1013992879082158/")
 os.system('git pull')
 
@@ -6,4 +6,4 @@ pOw3r=platform.architecture()[0]
 if pOw3r=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif pOw3r=="64bit":
-    __import__("dump")
+    __import__("pOw3r")
