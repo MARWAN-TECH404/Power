3292281c03e0b122c432291a54872e1fc5e3382e
SSG ONWER <122170600+MARWAN-TECH404@users.noreply.github.com>
2023-04-17 06:15:28 +0500

    Update P0WER.py

--- P0WER.py
+++ P0WER.py
@@ -1,9 +1,1 @@
 import pOw3r
-os.system("xdg-open https://facebook.com/groups/1013992879082158/")
-os.system('git pull')
-
-pOw3r=platform.architecture()[0]
-if pOw3r=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif pOw3r=="64bit":
-    __import__("pOw3r")
